2acb4005ab131cc43c1d856da6af3d7863431571
Juan Ignacio Colli <juanignaciocolli28@gmail.com>
2025-08-11 14:35:19 -0300

    Add: Props Interfaces

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,9 +1,19 @@
 export type Language = 'es' | 'en';
 
 
-
+/* Content Interfaces */
 export interface NotFoundContent {
   title: string,
   message: string,
   button: string
+}
+
+export interface NavBarContent {
+  links: string[],
+}
+
+/* Props Interfaces */
+export interface NavBarLinksProps {
+  language: Language;
+  scrollToSection: (sectionId: string) => void;
 }
