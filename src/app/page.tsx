11db02db4a1546b59717bf1c791eb1b4c3b342f6
Juan Ignacio Colli <juanignaciocolli28@gmail.com>
2025-09-22 14:23:53 -0300

    Update: styles home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Home from "@/pages/Home";
 
 export default function HomePage() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start bg-background">
+    <div className="font-sans grid min-h-screen pb-20 gap-16">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center justify-center bg-background">
         <Home/>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
